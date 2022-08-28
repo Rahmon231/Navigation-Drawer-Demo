6d96e711297d83785d6cd958a415a5d8a4730220
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2022-08-28 12:30:37 +0100

    ui setup

--- app/src/main/java/com/lemzeeyyy/navigationdrawerdemo/MainActivity.java
+++ app/src/main/java/com/lemzeeyyy/navigationdrawerdemo/MainActivity.java
@@ -1,14 +1,41 @@
 package com.lemzeeyyy.navigationdrawerdemo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawer = findViewById(R.id.drawer_layout);
+        nvDrawer = findViewById(R.id.nvView);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.drawer_layout:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
